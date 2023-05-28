0f2536420297d08b3de8cb2846ad8513fb97f820
Abdul Musavveer Alji <abdulmusavveeralji@gmail.com>
2023-05-28 23:01:23 +0530

    life cycle hooks 2

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,11 +1,22 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit, AfterViewChecked, AfterViewInit,
+  Component,
+  DoCheck,
+  EventEmitter,
+  Input,
+  OnChanges, OnDestroy,
+  OnInit,
+  Output
+} from '@angular/core';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent implements OnInit{
+export class PostsComponent implements OnInit, OnChanges, DoCheck,
+  AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy{
 
   // imgURL = 'https://picsum.photos/id/237/200/300'
   @Input() postImg = '';
@@ -17,4 +28,32 @@ export class PostsComponent implements OnInit{
   ngOnInit(): void {
     console.log('ngOnInit called', this.postImg)
   }
+
+  ngOnChanges(): void {
+    console.log('onchanges called')
+  }
+  ngDoCheck(): void {
+    console.log('docheck called')
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('contentchecked called')
+  }
+
+  ngAfterContentInit(): void {
+    console.log('content init called')
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('view Checked called')
+  }
+
+  ngAfterViewInit(): void {
+    console.log('view Init called')
+  }
+
+  ngOnDestroy(): void {
+    console.log('On destroy called')
+  }
+
 }
