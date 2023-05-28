35fce62d72147b5f61f7e2a7c0f42e0722c1be72
Abdul Musavveer Alji <abdulmusavveeralji@gmail.com>
2023-05-28 22:44:38 +0530

    life cycle hooks 1

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,13 +1,20 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit{
 
   // imgURL = 'https://picsum.photos/id/237/200/300'
   @Input() postImg = '';
   @Output() selectedImg = new EventEmitter();
+
+  constructor() {
+    console.log('constructor called', this.postImg)
+  }
+  ngOnInit(): void {
+    console.log('ngOnInit called', this.postImg)
+  }
 }
